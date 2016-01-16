24289e659e175dafd8bead88dbd6fb913b58ef03
rzhang0720 <rkz1@rice.edu>
2016-01-16 11:45:13 -0600

    Added basic camera feature to app. Click the "Camera" Button on the screen to open up the camera app. Take a picture, press the checkmark, and the picture should appear in an imageview in the middle of the screen.

--- app/src/main/java/com/colorid/MainActivity.java
+++ app/src/main/java/com/colorid/MainActivity.java
@@ -1,6 +1,9 @@
 package com.colorid;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -8,8 +11,14 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button cameraButton;
+    int REQUEST_CODE = 1;
+    ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +26,18 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        cameraButton = (Button) findViewById(R.id.camera_button);
+        imageView = (ImageView) findViewById(R.id.imageView);
+
+        cameraButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                if(intent.resolveActivity(getPackageManager())!=null){
+                    startActivityForResult(intent, REQUEST_CODE);
+                }
+            }
+        });
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -33,6 +54,18 @@ public class MainActivity extends AppCompatActivity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data){
+        if (requestCode == REQUEST_CODE){
+            if (resultCode == RESULT_OK){
+                Bundle bundle = new Bundle();
+                bundle = data.getExtras();
+                Bitmap bitMap = (Bitmap) bundle.get("data");
+                imageView.setImageBitmap(bitMap);
+            }
+        }
+
     }
 
     @Override
